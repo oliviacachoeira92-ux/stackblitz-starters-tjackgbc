6628ecebbd6aed0f55a1b8fadb057b896f394777
oliviacachoeira92-ux <olivia.cachoeira92@gmail.com>
2026-05-20 23:56:54 -0300

    fix add trailing slash support for iOS Safari routes

--- next.config.js
+++ next.config.js
@@ -2,3 +2,9 @@
 const nextConfig = {};
 
 module.exports = nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    trailingSlash: true,
+  };
+  
+  module.exports = nextConfig;
