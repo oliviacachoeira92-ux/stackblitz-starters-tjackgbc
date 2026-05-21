bf5ac5e6f66d57979ba92be559930462b97e12b6
oliviacachoeira92-ux <olivia.cachoeira92@gmail.com>
2026-05-21 00:11:15 -0300

    git commit -m "fix: resolve duplicated next config and iOS 404 issue"

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = nextConfig;
-/** @type {import('next').NextConfig} */
 const nextConfig = {
     trailingSlash: true,
   };
   
   module.exports = nextConfig;
-  
